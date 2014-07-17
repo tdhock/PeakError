8515213cd35f96180840b99a991961de3f7094be
Toby Dylan Hocking <tdhock5@gmail.com>
2014-07-17 10:58:28 -0400

    test other anns, other chroms

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,19 +1,58 @@
 context("Examples")
 
 test_that("1 peakStart region examples correct", {
-  chrom <- "chr5"
   regions <-
-    data.frame(chrom, chromStart=10, chromEnd=20, annotation="peakStart")
+    data.frame(chrom="chr5", chromStart=10, chromEnd=20, annotation="peakStart")
   expect_tp_fp <- function(peaks, tp, fp){
     err <- PeakError(peaks, regions)
     expect_equal(err$tp, tp)
     expect_equal(err$fp, fp)
   }
-  expect_tp_fp(Peaks(chrom, 15, 25), 1, 0)
+  expect_tp_fp(Peaks("chr5", 15, 25), 1, 0)
   expect_tp_fp(Peaks(), 0, 0)
-  expect_tp_fp(Peaks(chrom, 12, 15), 1, 0)
-  expect_tp_fp(Peaks(chrom, c(12, 18), c(15, 25)), 1, 1)
-  expect_tp_fp(Peaks(chrom, c(5, 15), c(12, 18)), 1, 1)
-  expect_tp_fp(Peaks(chrom, 5, 12), 0, 1)
-  expect_tp_fp(Peaks(chrom, 5, 25), 0, 1)
+  expect_tp_fp(Peaks("chr5", 20, 30), 0, 0)
+  expect_tp_fp(Peaks("chr12", 12, 15), 0, 0)
+  expect_tp_fp(Peaks("chr5", 12, 15), 1, 0)
+  expect_tp_fp(Peaks("chr5", c(12, 18), c(15, 25)), 1, 1)
+  expect_tp_fp(Peaks("chr5", c(5, 15), c(12, 18)), 1, 1)
+  expect_tp_fp(Peaks("chr5", 5, 12), 0, 1)
+  expect_tp_fp(Peaks("chr5", 5, 25), 0, 1)
+})
+
+test_that("1 peakEnd region examples correct", {
+  regions <-
+    data.frame(chrom="chr5", chromStart=10, chromEnd=20, annotation="peakEnd")
+  expect_tp_fp <- function(peaks, tp, fp){
+    err <- PeakError(peaks, regions)
+    expect_equal(err$tp, tp)
+    expect_equal(err$fp, fp)
+  }
+  expect_tp_fp(Peaks("chr5", 15, 25), 0, 1)
+  expect_tp_fp(Peaks(), 0, 0)
+  expect_tp_fp(Peaks("chr5", 20, 30), 0, 0)
+  expect_tp_fp(Peaks("chr12", 12, 15), 0, 0)
+  expect_tp_fp(Peaks("chr5", 12, 15), 1, 0)
+  expect_tp_fp(Peaks("chr5", c(12, 18), c(15, 25)), 1, 1)
+  expect_tp_fp(Peaks("chr5", c(5, 15), c(12, 18)), 1, 1)
+  expect_tp_fp(Peaks("chr5", 5, 12), 1, 0)
+  expect_tp_fp(Peaks("chr5", 5, 25), 0, 1)
+})
+
+test_that("1 noPeaks region examples correct", {
+  regions <-
+    data.frame(chrom="chr5", chromStart=10, chromEnd=20, annotation="noPeaks")
+  expect_tp_fp <- function(peaks, tp, fp){
+    err <- PeakError(peaks, regions)
+    expect_equal(err$tp, tp)
+    expect_equal(err$fp, fp)
+  }
+  expect_tp_fp(Peaks("chr5", 15, 25), 0, 1)
+  expect_tp_fp(Peaks(), 0, 0)
+  expect_tp_fp(Peaks("chr5", 20, 30), 0, 0)
+  expect_tp_fp(Peaks("chr12", 12, 15), 0, 0)
+  expect_tp_fp(Peaks("chr5", 12, 15), 0, 1)
+  expect_tp_fp(Peaks("chr5", c(12, 18), c(15, 25)), 0, 1)
+  expect_tp_fp(Peaks("chr5", c(5, 15), c(12, 18)), 0, 1)
+  expect_tp_fp(Peaks("chr5", 5, 12), 0, 1)
+  expect_tp_fp(Peaks("chr5", 5, 25), 0, 1)
 })
