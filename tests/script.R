bccb38c1eabdfe1d547b04303efee4b06019308c
Toby Dylan Hocking <tdhock5@gmail.com>
2016-02-15 10:53:14 -0500

    PeakError_

--- tests/script.R
+++ tests/script.R
@@ -1,8 +1,8 @@
 ## I initially tried writing these tests using testthat, but I got
 ## errors when using the system() and system2() functions (bug in
 ## testthat).
-compute_error.R <- system.file(
-  "exec", "compute_error.R",
+PeakError_compute.R <- system.file(
+  "exec", "PeakError_compute.R",
   mustWork=TRUE,
   package="PeakError")
 Rscript <- R.home(file.path("bin", "Rscript"))
@@ -11,9 +11,9 @@ peaks.bed <- file.path(exampleData, "peaks.bed")
 labels.bed <- file.path(exampleData, "labels.bed")
 overlapping.bed <- file.path(exampleData, "overlapping_peaks.bed")
 
-##test_that("compute_error.R correct", {
+##test_that("PeakError_compute.R correct", {
 out.file <- tempfile()
-cmd <- paste(Rscript, compute_error.R, peaks.bed, labels.bed, ">", out.file)
+cmd <- paste(Rscript, PeakError_compute.R, peaks.bed, labels.bed, ">", out.file)
 status <- system(cmd)
 errors <- read.table(out.file, sep="\t", header=TRUE)
 stopifnot(all.equal(
@@ -31,12 +31,12 @@ stopifnot(all.equal(
     1, 1, 0, 0,
     1, 0, 1, 0)))
 
-## summarize_error.R script
-summarize_error.R <- system.file(
-  "exec", "summarize_error.R",
+## PeakError_summarize.R script
+PeakError_summarize.R <- system.file(
+  "exec", "PeakError_summarize.R",
   mustWork=TRUE,
   package="PeakError")
-cmd <- paste(Rscript, summarize_error.R, out.file)
+cmd <- paste(Rscript, PeakError_summarize.R, out.file)
 out.vec <- system(cmd, intern=TRUE)
 out.txt <- paste(out.vec, collapse="\n")
 stopifnot(grepl("incorrect labels", out.txt))
@@ -44,7 +44,7 @@ stopifnot(grepl("false positive rate", out.txt))
 stopifnot(grepl("false negative rate", out.txt))
 
 ## Two arguments
-cmd <- paste(Rscript, summarize_error.R, out.file, out.file)
+cmd <- paste(Rscript, PeakError_summarize.R, out.file, out.file)
 out.vec <- system(cmd, intern=TRUE)
 out.txt <- paste(out.vec, collapse="\n")
 stopifnot(grepl("incorrect labels", out.txt))
@@ -52,7 +52,7 @@ stopifnot(grepl("false positive rate", out.txt))
 stopifnot(grepl("false negative rate", out.txt))
 
 ##test_that("overlapping_peaks.bed shows error", {
-arg.vec <- paste(compute_error.R, overlapping.bed, labels.bed)
+arg.vec <- paste(PeakError_compute.R, overlapping.bed, labels.bed)
 out.vec <- system2(Rscript, arg.vec, stderr=TRUE)
 out.txt <- paste(out.vec, collapse="\n")
 stopifnot(grepl("overlapping peaks", out.txt))
